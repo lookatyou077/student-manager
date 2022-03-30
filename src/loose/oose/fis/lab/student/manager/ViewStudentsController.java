32821b2d9c2122c1b7ad7e510afec80fe4429714
arnoldSi <a.simionica777@gmail.com>
2022-03-30 09:53:48 +0300

    All Bugs have been fixed. LogIn Screen works perfectly

--- src/loose/oose/fis/lab/student/manager/ViewStudentsController.java
+++ src/loose/oose/fis/lab/student/manager/ViewStudentsController.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import loose.oose.fis.lab.student.manager.model.Student;
+
 public class ViewStudentsController {
 
     @FXML
